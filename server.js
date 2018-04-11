47d7de0a1b82342164853fb838d81f2ff33a179e
Yuliya Volkova <Yuliyavolkova@gmail.com>
2018-04-11 08:03:14 +0300

    heroku server

--- server.js
+++ server.js
@@ -1,60 +1,3 @@
-/*const express = require('express');
-const path = require('path');
-const logger = require('morgan');
-const bodyParser = require('body-parser');
-require('./api/models/db');
-const mongoose = require('mongoose');
-
-const app = express();
-const index = require('./routes/index');
-const indexApi = require('./api/routes/index');
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
- app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
- });
-
-app.use('/myadmin(.html)?', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'myadmin.html'));
-});
-app.use('/api', indexApi);
-app.use('/', index);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-app.listen(3000, () => {
-  console.log('server is running on port: 3000');
-});*/
-
-//-----------
-//-----------
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
@@ -70,6 +13,7 @@ require('./config/db');
 const app = express();
 const index = require('./routes/index');
 const indexApi = require('./api/routes/index');
+app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -79,14 +23,6 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
- /*app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
- });*/
- 
 app.use(flash());
 app.use(session({
   key: 'mySession',
@@ -129,6 +65,6 @@ app.use(function (err, req, res, next) {
   res.render('error');
 });
 
-app.listen(3000, () => {
-  console.log('server is running on port: 3000');
+app.listen(app.get('port'), () => {
+  console.log('Node app is running on port', app.get('port'));
 });
