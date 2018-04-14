3d20ea0ae0746dec976de8730775a8d2d2d38fc2
Yuliya Volkova <Yuliyavolkova@gmail.com>
2018-04-14 04:07:28 +0300

    heroku

--- config/db.js
+++ config/db.js
@@ -28,8 +28,8 @@ router.post('/blog', isAuthenticated, ctrlBlog.createArticle); // CREATE
 router.put('/blog/:id', isAuthenticated, ctrlBlog.editArticle); // EDIT
 router.delete('/blog/:id', isAuthenticated, ctrlBlog.deleteArticle); // DELETE
 
-router.get('/slider', ctrlSlider.getSlides);
-router.post('/slider', ctrlSlider.addSlide);
+//router.get('/slider', ctrlSlider.getSlides);
+//router.post('/slider', ctrlSlider.addSlide);
 
 router.get('/skill', ctrlSkills.getSkills); // READ
 router.post('/skill', isAuthenticated, ctrlSkills.createSkill); // CREATE
@@ -106,7 +106,7 @@ router.get('/slider', (req, res) => {
       console.log(`в базе uploads найдены ${files}`);
     }
   });
-};
+});
 // @route GET /
 // @desc Loads form
 /*router.get('/api/slider', (req, res) => {
@@ -137,7 +137,7 @@ router.post('/slider', upload.single('file'), (req, res) => {
   // body...
   console.log(`post добавление слайда ${res.json({ file: req.file })}`);
     res.redirect('/admin');
-};
+});
 
 // @route POST /upload
 // @desc  Uploads file to DB
@@ -170,31 +170,3 @@ require('../api/models/blog');
 //require('../api/models/slider');
 require('../api/models/skills');
 require('../models/user');
-
-
-var isAuthenticated = function(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  res.status(401).json({message: 'Unauthorized', error: 401})
-};
-
-router.get('/blog', ctrlBlog.getArticles); // READ
-router.post('/blog', isAuthenticated, ctrlBlog.createArticle); // CREATE
-router.put('/blog/:id', isAuthenticated, ctrlBlog.editArticle); // EDIT
-router.delete('/blog/:id', isAuthenticated, ctrlBlog.deleteArticle); // DELETE
-
-router.get('/slider', ctrlSlider.getSlides);
-router.post('/slider', ctrlSlider.addSlide);
-
-router.get('/skill', ctrlSkills.getSkills); // READ
-router.post('/skill', isAuthenticated, ctrlSkills.createSkill); // CREATE
-router.put('/skill/:id', isAuthenticated, ctrlSkills.editSkill); // EDIT
-router.delete('/skill/:id', isAuthenticated, ctrlSkills.deleteSkill); // DELETE
-
-
-router.get('*', (req, res) => {
-  res.status(404).json({msg: 'Not found', err: 404});
-})
-
-module.exports = router;
