d0b7d0e3a8c7f8c6916aa516c8d5d889d4cfdf36
Yuliya Volkova <Yuliyavolkova@gmail.com>
2018-04-14 08:50:35 +0300

    heroku

--- api/controllers/slider.js
+++ api/controllers/slider.js
@@ -1,28 +1,40 @@
 const mongoose = require('mongoose');
 
-const crypto = require('crypto');
 const multer = require('multer');
 const GridFsStorage = require('multer-gridfs-storage');
 const Grid = require('gridfs-stream');
-
-const crypto = require('crypto');
 
 module.exports.getSlides = function (req, res) {
   let conn = mongoose.connection;
   let gfs = Grid(conn.db, mongoose.mongo);
   gfs.collection('uploads');
+  const mesDefault = [{
+    title: 'В базе нет работ',
+    technologies: 'Frontend',
+    image: {}
+  }];
   gfs.files.find().toArray((err, files) => {
-    // Check if files
-    if (!files || files.length === 0) {
-      return res.status(404).json({
-        err: 'No files exist'
-      });
-    }
 
-    // Files exist
-     console.log(res.json(files));
-     
-    return res.json(files);
+    if (!files || files.length === 0) {
+      res
+          .status(200)
+          .json({dataArr: mesDefault});
+    } else {
+      files.map(file => {
+        if (
+          file.contentType === 'image/jpeg' ||
+          file.contentType === 'image/png'
+        ) {
+          file.isImage = true;
+        } else {
+          file.isImage = false;
+        }
+      });
+      res
+          .status(200)
+          .json({dataArr: mesDefault});
+      console.log(res.json(files));
+    }
   });
 };
 
