ad79c968dbad043764cc54120bbca34937fae81f
Yuliya Volkova <Yuliyavolkova@gmail.com>
2018-04-16 06:35:11 +0300

    heroku

--- controllers/blog.js
+++ controllers/blog.js
@@ -3,6 +3,8 @@ const config = require('../config/config.json');
 const apiServer = config.server.path;
 
 module.exports.blog = function (req, res) {
+   console.log('in control blog');
+   
   const pathAPI = config.server.blog;
   const requestOptions = {
     url: apiServer + pathAPI,
@@ -17,6 +19,5 @@ module.exports.blog = function (req, res) {
       console.log(error);
     }
     res.render('my_pages/blog', Object.assign({}, sendObj, body));
-    res.redirect('/blog');
   });
 }
